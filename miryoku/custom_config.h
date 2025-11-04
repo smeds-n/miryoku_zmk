9e887f48828b94217ee40b6d60b17c5e31b666a4
smeds-n <niklas.smeds@protonmail.com>
2025-11-04 22:24:37 +0200

    change layout to puuteri and add key_repeat without &

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,8 +5,7 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
-&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &mo U_FUN,         &mo U_NAV,         &kp SPACE,         &kp LSHFT,         &mo U_NUM,         &mo U_SYM,         U_NP,              U_NP
-
+&kp V,             &kp R,             &kp D,             &kp B,             &kp Q,             &kp COMMA,             &kp P,             &kp O,             &kp U,             &kp Y,             \
+U_MT(LGUI, S),     U_MT(LALT, N),     U_MT(LCTRL, T),    U_MT(LSHFT, H),    &kp K,             &kp G,             U_MT(LSHFT, C),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp M,             &kp L,             &kp J,             &kp W,             &kp F,             &kp APOS,         U_MT(RALT, SEMI),   U_LT(U_BUTTON, DOT),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, key_repeat),  U_NP,              U_NP
