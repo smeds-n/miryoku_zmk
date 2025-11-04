32a56499fca08a44e8c1c21b3600d4144f9269bf
smeds-n <niklas.smeds@protonmail.com>
2025-11-04 21:47:49 +0200

    add custom layer config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &mo U_FUN,         &mo U_NAV,         &kp SPACE,         &kp LSHFT,         &mo U_NUM,         &mo U_SYM,         U_NP,              U_NP
+
