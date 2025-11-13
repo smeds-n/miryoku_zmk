22b0d56609da2cd31f1c6bcef480c81cecbca592
smeds-n <128667634+smeds-n@users.noreply.github.com>
2025-11-13 21:45:05 +0200

    w and comma swap, added double quotes to keymap

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,9 +5,9 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_BASE \
-&kp V,             &kp R,             &kp D,             &kp B,             &kp Q,             &kp COMMA,             &kp P,             &kp O,             &kp U,             &kp Y,             \
+&kp V,             &kp R,             &kp D,             &kp B,             &kp Q,             &kp W,             &kp P,             &kp O,             &kp U,             &kp Y,             \
 U_MT(LGUI, S),     U_MT(LALT, N),     U_MT(LCTRL, T),    U_MT(LSHFT, H),    &kp K,             &kp G,              U_MT(LSHFT, C),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp M,             &kp L,             &kp J,             U_LT(U_FUN, W),             &kp F,          &kp RA(Q),         U_MT(RALT, RA(P)),   U_LT(U_BUTTON, DOT),\
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp M,             &kp L,             &kp J,             U_LT(U_FUN, COMMA),             &kp F,          &kp RA(Q),         U_MT(RALT, RA(P)),   U_LT(U_BUTTON, DOT),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),    U_LT(U_SYM, BSPC),  &u_lt2 U_NUM N0, U_LT(U_NUM, RET),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
@@ -18,7 +18,7 @@ U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp 
 
 
 #define MIRYOKU_LAYER_SYM \
-&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,      &kp SEMI,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,      &kp DOUBLE_QUOTE,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,     &kp SLASH,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
